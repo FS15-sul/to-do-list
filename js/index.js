c042a5682c62a5b27aea4d550ae2e2225662942a
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-08-19 12:08:36 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -4,10 +4,11 @@ function novaTarefa(){
     criar.classList.add("active");
 }
 
-function confirmarExclusao(){
+function confirmarExclusao(id){
     overlay.classList.add("active");
     modal.classList.add("active");
     confirmacao.classList.add("active");
+    deleteID = id;
 }
 
 function fecharModal(){
@@ -23,28 +24,37 @@ document.addEventListener('keyup', (evento) => {
     }   
 });
 
-let tarefas = [
-    {
-        titulo: "Pendurar quadro",
-        descricao: "Furar dois furos na parede para colocar as buchas e pendurar o quadro"
-    },
-    {
-        titulo: "Comer",
-        descricao: "Em casa"
-    },
-    {
-        titulo: "Reunião com cliente",
-        descricao: "Ir na KF veiculos se reunir com fabricio"
-    },
-    {
-        titulo: "Estudar",
-        descricao: "Entender melhor variaveis e constantes"
-    },
-    {
-        titulo: "Comprar capacete",
-        descricao: "Ir na loja e comprar o capacete que EU já escolhi e não ceder ao consumismo"
-    },
-];
+// let tarefas = [
+//     {
+//         id: 1,
+//         titulo: "Pendurar quadro",
+//         descricao: "Furar dois furos na parede para colocar as buchas e pendurar o quadro"
+//     },
+//     {
+//         id: 2,
+//         titulo: "Comer",
+//         descricao: "Em casa"
+//     },
+//     {
+//         id: 3,
+//         titulo: "Reunião com cliente",
+//         descricao: "Ir na KF veiculos se reunir com fabricio"
+//     },
+//     {
+//         id: 4,
+//         titulo: "Estudar",
+//         descricao: "Entender melhor variaveis e constantes"
+//     },
+//     {
+//         id: 5,
+//         titulo: "Comprar capacete",
+//         descricao: "Ir na loja e comprar o capacete que EU já escolhi e não ceder ao consumismo"
+//     },
+// ];
+
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+let deleteID = 0;
 
 function listarTarefas(){
     lista.innerHTML = "";
@@ -56,7 +66,7 @@ function listarTarefas(){
                     <p>${tarefa.descricao}</p>
                 </div>
                 <div>
-                    <box-icon name='trash' onclick="confirmarExclusao()"></box-icon>
+                    <box-icon name='trash' onclick="confirmarExclusao(${tarefa.id})"></box-icon>
                 </div>
             </li>
         `;
@@ -68,10 +78,25 @@ listarTarefas();
 function adicionarTarefa(){
     event.preventDefault();
     let tarefa = {
+        id: tarefas.length,
         titulo: titulo.value,
         descricao: descricao.value
     }
     tarefas.push(tarefa);
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+    fecharModal();
+    listarTarefas();
+}
+
+function deletarTarefa(){
+    let tarefasFiltradas = [];
+    tarefasFiltradas = tarefas.filter(tarefa => {
+        if(tarefa.id != deleteID){
+            return tarefa;
+        }
+    });
+    tarefas = tarefasFiltradas;
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
     fecharModal();
     listarTarefas();
 }
